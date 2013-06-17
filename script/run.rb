a51cd4092f61d6c1df765cce73ca1c6da50f6619
William Wedler <wwedler@aquion-energy.com>
2013-06-17 09:14:19 -0400

    gemfile path for updating

--- script/run.rb
+++ script/run.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+$0 = "cad_plugins"
+
+gemfile_path = File.join(File.dirname(__FILE__), '..', 'Gemfile')
+# Dir.chdir File.dirname(gemfile_path)
+# ENV['BUNDLE_GEMFILE'] = gemfile_path
+
 # Load the bundled environment
 require 'rubygems'
 require 'bundler/setup'
