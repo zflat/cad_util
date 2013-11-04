0815648545b60ed21748b97e35401e379b1b1cd8
William Wedler <wwedler@aquion-energy.com>
2013-11-04 14:50:08 -0500

    hidden or shown dispatcher switch default to hidden if no argument supplied

--- PluginClient/proc.bat
+++ PluginClient/proc.bat
@@ -1,1 +1,15 @@
-start "Dispatcher" ruby H:\sandbox\cad_util\script\run.rb stdout
+@echo off
+set /a hidden=1
+IF NOT %1.==. (
+set /a hidden=%1
+)
+set name="util_launcher"
+set bin=util_launcher
+set script=H:\sandbox\cad_util\script\run.rb
+set optns=stdout
+
+IF %hidden% EQU 1 (
+start %name% /B %bin% %script% %optns%
+) ELSE (
+start %name% %bin% %script% %optns%
+)
